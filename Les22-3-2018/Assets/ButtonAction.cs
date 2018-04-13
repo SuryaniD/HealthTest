1220ad8494cb7a895ef6b967655c3ea87742dab0
Suryani <bytebytermail@gmail.com>
2018-04-13 12:05:05 +0200

    Add existing file

---
+++ Les22-3-2018/Assets/ButtonAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour {
+
+	public void Onclick() {
+	transform.localScale += new Vector3(0.2f, 0.0f, 0.0f);
+	}
+
+
+	public void OnclickKrimp() {
+		transform.localScale -= new Vector3(0.2f, 0.0f, 0.0f);
+	}
+
+}
+
